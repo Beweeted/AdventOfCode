daa2ed4aa06f480a9439304da0a3b2ac33402f49
Beweeted <Beweeted@users.noreply.github.com>
2023-12-25 16:23:01 -0800

    2019-1b TDD

--- 2019/tdd-python/1/mycode.py
+++ 2019/tdd-python/1/mycode.py
@@ -3,7 +3,17 @@ import math
 
 def get_fuel(mass):
     fuel = math.floor(mass / 3)-2
+    fuel = max(fuel, 0)
     return fuel
+
+
+def get_recursive_fuel(mass):
+    total_fuel = get_fuel(mass)
+    extra_fuel = get_fuel(total_fuel)
+    while extra_fuel != 0:
+        total_fuel += extra_fuel
+        extra_fuel = get_fuel(extra_fuel)
+    return total_fuel
 
 
 def get_input(filename):
@@ -21,13 +31,28 @@ def sum_fuel(mass_list):
     return fuel_sum
 
 
-def main(mass_file):
+def sum_fuel_recursive(mass_list):
+    fuel_sum = 0
+    for mass in mass_list:
+        fuel_sum += get_recursive_fuel(mass)
+    return fuel_sum
+
+
+def simple_main(mass_file):
     mass_input = get_input(mass_file)
     total_fuel = sum_fuel(mass_input)
-    output = f'Total fuel required: {total_fuel}'
+    output = f'Simple fuel required: {total_fuel}'
+    return output
+
+
+def recursive_main(mass_file):
+    mass_input = get_input(mass_file)
+    total_fuel = sum_fuel_recursive(mass_input)
+    output = f'Recursive fuel required: {total_fuel}'
     return output
 
 
 if __name__ == '__main__':
     input_file = 'input.txt'
-    print(main(input_file))
+    print(simple_main(input_file))
+    print(recursive_main(input_file))
--- 2019/tdd-python/1/mytests.py
+++ 2019/tdd-python/1/mytests.py
@@ -10,12 +10,20 @@ class MyTests(unittest.TestCase):
         self.assertEqual(get_fuel(1969), 654)
         self.assertEqual(get_fuel(100756), 33583)
 
+    def test_recursive_fuel(self):
+        self.assertEqual(get_recursive_fuel(12), 2)
+        self.assertEqual(get_recursive_fuel(14), 2)
+        self.assertEqual(get_recursive_fuel(1969), 966)
+        self.assertEqual(get_recursive_fuel(100756), 50346)
+
     def test_import(self):
         self.assertEqual(len(get_input('sample.txt')), 4)
         self.assertEqual(get_input('sample.txt')[2], 1969)
 
     def test_fuel_sum(self):
         self.assertEqual(sum_fuel(get_input('sample.txt')), 34241)
+        self.assertEqual(sum_fuel_recursive(get_input('sample.txt')), 51316)
 
     def test_main(self):
-        self.assertEqual(main('sample.txt'), 'Total fuel required: 34241')
+        self.assertEqual(simple_main('sample.txt'), 'Simple fuel required: 34241')
+        self.assertEqual(recursive_main('sample.txt'), 'Recursive fuel required: 51316')
