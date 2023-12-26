7d66b6fcd4c92342ef94feeeecc430e8e140ec0f
Beweeted <Beweeted@users.noreply.github.com>
2023-12-25 19:33:13 -0800

    Refactored core functions for readability and usability

--- 2019/tdd-python/2/mycode.py
+++ 2019/tdd-python/2/mycode.py
@@ -6,30 +6,26 @@ def run_intcode(intcode):
         if instruction == 99:
             break
         elif instruction == 1:
-            add_values_and_store(intcode, position+1, position+2, position+3)
+            add_values_and_store(intcode, position+1)
         elif instruction == 2:
-            multiply_values_and_store(intcode, position+1, position+2, position+3)
+            multiply_values_and_store(intcode, position+1)
         position += 4
     return intcode[0]
 
 
-def add_values_and_store(intcode, position1, position2, position3):
-    loc1 = intcode[position1]
-    val1 = intcode[loc1]
-    loc2 = intcode[position2]
-    val2 = intcode[loc2]
-    dest = intcode[position3]
-    value = val1 + val2
-    print(f'Adding: {val1=}, {val2=}, {dest=}, {value=}')
-    intcode[dest] = value
+def add_values_and_store(intcode, idx):
+    val1 = intcode[intcode[idx]]
+    val2 = intcode[intcode[idx+1]]
+    dest = intcode[idx+2]
+    store = val1 + val2
+    print(f'Adding: {val1=}, {val2=}, {dest=}, {store=}')
+    intcode[dest] = store
 
 
-def multiply_values_and_store(intcode, position1, position2, position3):
-    loc1 = intcode[position1]
-    val1 = intcode[loc1]
-    loc2 = intcode[position2]
-    val2 = intcode[loc2]
-    dest = intcode[position3]
+def multiply_values_and_store(intcode, idx):
+    val1 = intcode[intcode[idx]]
+    val2 = intcode[intcode[idx+1]]
+    dest = intcode[idx+2]
     value = val1 * val2
     print(f'Multiplying: {val1=}, {val2=}, {dest=}, {value=}')
     intcode[dest] = value
--- 2019/tdd-python/2/mytests.py
+++ 2019/tdd-python/2/mytests.py
@@ -11,13 +11,13 @@ class MyTestCase(unittest.TestCase):
 
     def test_intcode_add_and_store(self):
         intcode = get_intcode('sample.txt')
-        add_values_and_store(intcode, 5, 6, 2)
-        self.assertEqual(intcode[10], 53)
+        add_values_and_store(intcode, 5)
+        self.assertEqual(intcode[0], 53)
 
     def test_intcode_multiply_and_store(self):
         intcode = get_intcode('sample.txt')
-        multiply_values_and_store(intcode, 5, 6, 2)
-        self.assertEqual(intcode[10], 150)
+        multiply_values_and_store(intcode, 5)
+        self.assertEqual(intcode[0], 150)
 
     def test_run_intcode_program(self):
         intcode = get_intcode('sample.txt')
