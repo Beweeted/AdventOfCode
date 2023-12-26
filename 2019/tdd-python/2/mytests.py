37a8ce0004733b3269217b0c06a08e496351035d
Beweeted <Beweeted@users.noreply.github.com>
2023-12-25 18:45:03 -0800

    Adding addition.

--- 2019/tdd-python/2/mytests.py
+++ 2019/tdd-python/2/mytests.py
@@ -11,6 +11,7 @@ class MyTestCase(unittest.TestCase):
     def test_intcode_addition(self):
         intcode = get_intcode('sample.txt')
         self.assertEqual(add_values(intcode, 4, 8), 101)
+        self.assertEqual(add_values(intcode, 1, 10), 49)
 
 
 if __name__ == '__main__':
